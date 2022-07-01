bf3c76cd8d4dc4cddc9991cafd42a145d47af19f
wyp <834287243@qq.com>
2022-07-01 14:38:33 +0800

    tesgt

--- chapter03/demo01/test.go
+++ chapter03/demo01/test.go
@@ -21,7 +21,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
     mu.Lock()
     count++
     mu.Unlock()
-    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)  
 }
 
 // counter echoes the number of calls so far.
